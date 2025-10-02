63d4b15597f26081b0487f19f26ff9315c8d01ca
VanHoutteTibo <tibo.van.houtte@student.howest.be>
2025-10-02 17:12:48 +0200

    Initial commit + PresentationAccessorService

---
+++ PresentationAccessorService.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+
+public class PresentationAccessorService {
+
+    private final Accessor accessor;
+
+    public PresentationAccessorService(Accessor accessor)
+    {
+        this.accessor = accessor;
+    }
+
+    public void loadFile(Presentation presentation, String fn) throws IOException {
+        this.accessor.loadFile(presentation, fn);
+    }
+
+    public void saveFile(Presentation presentation, String fn) throws IOException {
+        this.accessor.saveFile(presentation, fn);
+    }
+}
